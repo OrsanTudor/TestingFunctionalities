8b84d1cf2cd868f85a78962451a558df4b22e62d
DESKTOP-CU71783\Asus <tudor.orsan@yahoo.com>
2025-04-30 16:57:18 +0300

    Testing 2

--- page.js
+++ page.js
@@ -1,3 +1,26 @@
+// When the button is clicked, show the message
 document.getElementById("clickMeBtn").addEventListener("click", () => {
-  document.getElementById("message").textContent = "Thanks for clicking!";
+  document.getElementById("message").textContent = "You clicked the button!";
 });
+
+// Modal functionality
+var modal = document.getElementById("myModal");
+var modalBtn = document.getElementById("modalBtn");
+var closeModal = document.getElementsByClassName("close")[0];
+
+// Show the modal
+modalBtn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// Close the modal when the user clicks on <span> (x)
+closeModal.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Close the modal if the user clicks outside of it
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
